c7a4b62856bbf4c4fd75d153cd40d5042d64255d
ou <3250679635@qq.com>
2021-08-08 12:08:21 +0800

    为登录模块div添加圆角属性

--- src/component/Login.js
+++ src/component/Login.js
@@ -8,7 +8,8 @@ const Login = () => {
             <div className="ui center aligned middle aligned grid" 
                     style={{height:"100vh"}}>
                 <div className="column" style={{maxWidth:"450px"}}>
-                    <div className="ui segment">
+                    <div className="ui segment" 
+                        style={{display:"block",borderRadius:"20px"}}>
                         <h2 className="ui orange center aligned header">
                             <SunFlower/>
                         </h2>
