523349e0f00ba09a5aa085b982721c23b5dccaa0
ou <3250679635@qq.com>
2021-08-09 13:00:19 +0800

    几乎完成了主页面的全部设计

--- src/component/MainPage/main.js
+++ src/component/MainPage/main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import "./main.css"
 import MainPageHeader from './MainPageHeader';
+import MainContent from './MainContent';
 import Fooster from './footer';
 
 const Main = () =>{
     return(
         <div className="lms-app">
             <MainPageHeader/>
+            <MainContent/>
             <Fooster/>
         </div>
     )
