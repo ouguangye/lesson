3f09545c59033b043c2f6b36c6aafacaf0ecf1c5
ou <3250679635@qq.com>
2021-08-12 09:40:45 +0800

    实现了取消按钮的功能

--- src/component/createLessons/Button.js
+++ src/component/createLessons/Button.js
@@ -1,13 +1,21 @@
+import history from "../history"
+
+onButtonClick = () => {
+    
+}
+
 const Button = () => {
     return(
         <div className="fields" style={{float:"right"}}>
             <button type="submit" className="ui blue button"
-               style={{marginRight: "8px"}} >
+               style={{marginRight: "8px"}}
+                >
                    <i aria-hidden="true" className="check icon"></i>
                    确认
             </button>
             <button type="button" className="ui small button"
-               style={{marginRight: "8px"}} >
+               style={{marginRight: "8px"}} 
+               onClick={()=>{history.push("/main")}} >
                    <i aria-hidden="true" className="cancel icon"></i>
                    取消
             </button>
