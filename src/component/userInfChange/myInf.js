1a0f35744a71b2ebf7418e1d32ac0b82f4ef1bae
ou <3250679635@qq.com>
2021-08-13 16:33:11 +0800

    完成所有功能

--- src/component/userInfChange/myInf.js
+++ src/component/userInfChange/myInf.js
@@ -4,8 +4,29 @@ import InfPart1 from './InfPart1';
 import InfPart2 from './InfPart2';
 import InfPart3 from './InfPart3';
 import history from '../history';
+import { connect } from 'react-redux';
+import lms from '../../api/lms';
+import { dataRequest } from "../../actions";
 
-const MyInf = () => {
+
+const MyInf = props => {
+    const changeSumit = async(e) => {
+        e.preventDefault();
+        await lms.post(
+            "/json/account/save",{},
+            {
+                params:{
+                    name: props.name,
+                    email: props.email,
+                    phone: props.phone,
+                    introduction: props.introduction
+                }
+            }
+        )
+        props.dataRequest();
+        history.push("/main/lesson");
+    }
+
     return (
         <React.Fragment>
             <br/><br/><br/><br/>
@@ -17,10 +38,12 @@ const MyInf = () => {
                             <InfPart1/>
                             <InfPart2/>
                             <InfPart3/>
-                            <button type="submit" className="ui primary button">
+                            <button 
+                             className="ui primary button"
+                             onClick={e=>{changeSumit(e)}} >
                                 更新个人信息
                             </button>
-                            <button type="submit" className="ui cancel button"
+                            <button  className="ui cancel button"
                                onClick ={()=>{history.push("/main/lesson")}}  >
                                 取消
                             </button>
@@ -32,4 +55,13 @@ const MyInf = () => {
     )
 }
 
-export default MyInf;
+const mapStateToProps = state =>{
+    return{
+        name: state.data.userName,
+        email: state.data.email,
+        phone: state.data.phone,
+        introduction: state.data.introduction
+    }
+}
+
+export default connect(mapStateToProps,{dataRequest})(MyInf);
