a27c5bde14071628145278489db3c5d39c43ed90
ou gunagye <3250679635@qq.com>
2021-08-15 23:12:41 +0800

    修改了mainpageHeader的css修改

--- src/component/MainPage/MainPageHeader.js
+++ src/component/MainPage/MainPageHeader.js
@@ -2,9 +2,18 @@ import React from 'react';
 import BreadCrumb from './breadcrumb';
 import ListBox from './listBox';
 
+
+const style ={
+    backgroundColor:"transparent",
+    border:"0px",
+    boxShadow:"none",
+    color: "white",
+    height:"56px"
+}
+
 const MainPageHeader = () =>{
     return(
-        <div className="ui borderless top fixed menu HeaderMenu">
+        <div className="ui borderless top fixed menu" style={style}>
             <div className="ui container" 
                 style={{display: "flex", justifyContent: "space-between"}}>
                     <BreadCrumb/>
--- src/component/MainPage/Menu.js
+++ src/component/MainPage/Menu.js
@@ -2,10 +2,17 @@ import React from 'react';
 import lms from '../../api/lms';
 import history from '../history';
 
+const itemStyle = {
+    fontSize: "14px",
+    cursor: "pointer",
+    fontWeight:"200"
+} 
+
 const Menu = props => {
     return(
         <React.Fragment>
-            <div role="option" className="item" aria-selected="false" >
+            <div role="option" className="item" 
+                aria-selected="false" style={itemStyle} >
                 <i aria-hidden="true" className="user icon"></i>
                 <a href="#/" style={{color:"black"}} 
                     onClick={(e)=>{
@@ -16,11 +23,13 @@ const Menu = props => {
                     个人信息
                 </a>
             </div>
-            <div role="option" className="item" aria-selected="false">
+            <div role="option" className="item" 
+                aria-selected="false" style={itemStyle}>
                 <i aria-hidden="true" className="wrench icon"></i>
                 <a href="#/" style={{color:"black"}}>管理端</a>
             </div>
-            <div role="option" className="item" aria-selected="false">
+            <div role="option" className="item" 
+                aria-selected="false" style={itemStyle}>
                 <i aria-hidden="true" className="sign out icon"></i>
                 <a href="#/" style={{color:"black"}} 
                     onClick={()=>{signOut();}}>
