5faa19ff7e874785b52104db0fe851a6eeb8f4a0
ou gunagye <3250679635@qq.com>
2021-08-15 22:53:54 +0800

    修改一下Login页面的css

--- src/component/LoginIn/Login.js
+++ src/component/LoginIn/Login.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import SunFlower from '../sunflower';
 import Form from './Form';
-import "./login.css"
+
+const style ={
+    position:"absolute",
+    width: "100%",
+    height: "100%",
+    background: "url('http://lms.scutnc.cn/static/media/login_bg.a5b3d58b.jpg') 100% center / 100% no-repeat"
+}
 
 class Login extends React.Component {
     render(){
         return(
-            <div className="loginDiv">
+            <div style={style}>
                 <div className="ui center aligned middle aligned grid" 
                         style={{height:"100vh"}}>
                     <div className="column" style={{maxWidth:"450px"}}>
